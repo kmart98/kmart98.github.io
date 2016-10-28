ac9ca0120c84e6b7aa7d7476fb4899b649cdcedc
kmart98 <kylemartinez91798@gmail.com>
2016-10-28 12:23:37 -0700

    this is just a test

---
+++ Homework Assignment #3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+const double FIFTEEN_PERCENT = .15;
+const double TWENTY_EIGHT = .28;
+const double THIRTY_EIGHT = .38;
+
+int main()
+{
+
+}
